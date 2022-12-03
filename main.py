5e725f2b6d45e2f46f20067a81c00daab3d91f18
Skyblade64 <pfarley64@gmail.com>
2022-12-03 20:51:35 +0000

    Finished ReceptionByGenre having weird issues with poetry

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
 from typing import List
-
+import requests as rq
 import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+
+def GamesInGenre(genre): #returns dataframe 
+    response = rq.get("http://steamspy.com/api.php?request=genre&genre="+genre)
+    data = (
+        response.json()
+    )  # based on this https://atcoordinates.info/2019/09/24/examples-of-using-the-census-bureaus-api-with-python/
+    #print(data)
+    df = pd.DataFrame(data)
+    return df
+def SumReviews(games):
+    pos=games.loc['positive'].sum()
+    neg = games.loc['negative'].sum()
+    return [pos,neg]
+def ReceptionByGenre(genre):
+    games=GamesInGenre(genre)
+    reviews=SumReviews(games)
+    #positive=reviews[0]
+    #negative=reviews[1]
+    data = np.array(reviews)
+    sentiment=["Positive","Negative"]
+    plt.title("Sentiments of "+genre+" Games")
+    plt.pie(data, labels = sentiment,autopct='%1.1f%%')
+    plt.show()
+
+
+#ReceptionByGenre("Early+Access")
