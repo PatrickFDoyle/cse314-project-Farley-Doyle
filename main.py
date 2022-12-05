390898d1f5aac493af031e0f775aeacf50589446
Skyblade64 <pfarley64@gmail.com>
2022-12-05 23:30:18 +0000

    beta version of PublisherGraph

--- main.py
+++ main.py
@@ -26,6 +26,45 @@ def ReceptionByGenre(genre):
     plt.title("Sentiments of "+genre+" Games")
     plt.pie(data, labels = sentiment,autopct='%1.1f%%')
     plt.show()
+def Top100(): #returns the top 100 steam games of all time
+    response = rq.get("http://steamspy.com/api.php?request=top100forever")
+    data = (
+        response.json()
+    )  # based on this https://atcoordinates.info/2019/09/24/examples-of-using-the-census-bureaus-api-with-python/
+    #print(data)
+    df = pd.DataFrame(data)
+    #print(df.index)
+    #publishers = (df.loc['publisher'].unique())
+    #print(len(publishers))
+    return df
+def PublisherGraph(games):
+    #publishers = (games.loc['publisher'].unique())
+    publishers=(games.loc['publisher']).values.tolist()
+    cleanedpubs=[]
+    for publisher in publishers:
+        cleaned=publisher.split(',')
+        for values in cleaned:
+            cleanedpubs.append(values)
+    #print(cleanedpubs)
+    publisherdict={}
+    #publisherdict['other']=0
+    for publisher in cleanedpubs:
+        num=cleanedpubs.count(publisher)
+        if(num>1):
+            publisherdict[publisher]=cleanedpubs.count(publisher)
+        #else:
+        #    publisherdict['other']+=1
+    if ' Inc.' in publisherdict:
+        publisherdict.pop(' Inc.')
+    if ' Ltd.' in publisherdict:
+        publisherdict.pop(' Ltd.')
+    uniquepublishers = publisherdict.keys()
+    plt.title("Publishers frequency in top 100 steam games")
+    plt.bar(uniquepublishers,publisherdict.values())
+    plt.xticks(fontsize=14,rotation = 90)
+    plt.show()
 
 
+games=Top100()
+PublisherGraph(games)
 #ReceptionByGenre("Early+Access")
